9c123f11ad452433a60c711d4425e73c363f053c
AxelH97 <hilde.axel@web.de>
2023-11-13 13:07:27 +0100

    main updadet

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let currTile;
 let otherTile;
 let turns = 0;
 let timerExpired = false;
+
 function moveStart() {
   if (this.style.border.includes("green")) {
     return;
@@ -11,13 +12,17 @@ function moveStart() {
   currTile = this;
   this.style.border = "1px solid black";
 }
+
 function moveOver(e) {
   e.preventDefault();
 }
+
 function moveEnter(e) {
   e.preventDefault();
 }
+
 function moveLeave() {}
+
 function moveDrop() {
   otherTile = this;
 }
@@ -33,6 +38,7 @@ function moveEnd() {
   document.getElementById("turns").innerText = turns;
   checkImagePlacement(otherTile);
 }
+
 function checkImagePlacement(image) {
   const imageName = image.src.split("/").pop();
   const imageOrder = parseInt(imageName.split(".")[0], 10);
@@ -46,13 +52,13 @@ function checkImagePlacement(image) {
     image.removeEventListener("dragend", moveEnd);
     image.removeEventListener("dragover", moveOver);
     image.removeEventListener("dragenter", moveEnter);
-    image.removeEventListener("dragleave", moveLeave);
     image.removeEventListener("drop", moveDrop);
   } else {
     image.style.border = "1.5px solid red";
     // Bild ist an der falschen Stelle
   }
 }
+
 window.onload = function () {
   for (let r = 0; r < rows; r++) {
     for (let c = 0; c < columns; c++) {
@@ -84,6 +90,7 @@ window.onload = function () {
     document.getElementById("pieces").append(tile);
   }
 };
+
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -155,6 +162,7 @@ function toggleImage() {
   }
   isImageVisible = !isImageVisible;
 }
+
 toggleButton.addEventListener("mousedown", toggleImage);
 toggleButton.addEventListener("mouseup", toggleImage);
 function allImagesPlacedCorrectly() {
@@ -166,103 +174,6 @@ function allImagesPlacedCorrectly() {
   }
   return true; // Alle Bilder sind korrekt platziert
 }
-
-function moveStart() {
-  currTile = this;
-}
-
-function moveOver(e) {
-  e.preventDefault();
-}
-
-function moveEnter(e) {
-  e.preventDefault();
-}
-
-function moveLeave() {}
-
-function moveDrop() {
-  otherTile = this;
-}
-
-function moveEnd() {
-  if (currTile.src.includes("blank")) {
-    return;
-  }
-  let currImg = currTile.src;
-  let otherImg = otherTile.src;
-  currTile.src = otherImg;
-  otherTile.src = currImg;
-
-  turns += 1;
-  document.getElementById("turns").innerText = turns;
-}
-
-window.onload = function () {
-  for (let r = 0; r < rows; r++) {
-    for (let c = 0; c < columns; c++) {
-      let tile = document.createElement("img");
-      tile.src = "./images/annie.jpg";
-
-      tile.addEventListener("dragstart", moveStart);
-      tile.addEventListener("dragover", moveOver);
-      tile.addEventListener("dragenter", moveEnter);
-      tile.addEventListener("dragleave", moveLeave);
-      tile.addEventListener("drop", moveDrop);
-      tile.addEventListener("dragend", moveEnd);
-
-      document.getElementById("board").append(tile);
-    }
-  }
-
-  let pieces = [];
-  for (let i = 1; i <= rows * columns; i++) {
-    pieces.push(i.toString());
-  }
-  pieces.reverse();
-
-  for (let i = 0; i < pieces.length; i++) {
-    let tile = document.createElement("img");
-    tile.src = "./images/" + pieces[i] + ".jpg";
-
-    tile.addEventListener("dragstart", moveStart);
-    tile.addEventListener("dragover", moveOver);
-    tile.addEventListener("dragenter", moveEnter);
-    tile.addEventListener("dragleave", moveLeave);
-    tile.addEventListener("drop", moveDrop);
-    tile.addEventListener("dragend", moveEnd);
-
-    document.getElementById("pieces").append(tile);
-  }
-};
-
-function startTimer(duration) {
-  var timer = duration,
-    minutes,
-    seconds;
-  var interval = setInterval(function () {
-    minutes = parseInt(timer / 60, 10);
-    seconds = parseInt(timer % 60, 10);
-    document.getElementById("timer").innerText =
-      minutes + "m " + seconds + "s ";
-    if (--timer < 0) {
-      timer = duration;
-      clearInterval(interval);
-      timerExpired = true; // Timer ist abgelaufen
-      document.getElementById("message").innerText = "You lose, Time is up!";
-      document.getElementById("message").style.display = "block";
-      removeDragEventListeners();
-    } else if (allImagesPlacedCorrectly()) {
-      clearInterval(interval);
-      document.getElementById("message").innerText = "You win!";
-      document.getElementById("message").style.display = "block";
-      removeDragEventListeners();
-    }
-
-    document.getElementById("timerSound").play("bing.mp3");
-  }, 1000);
-}
-startTimer(300); // Zeit wariabel einstellen um das spiel zu verlängern oder zu verkürzen
 
 function playSound() {
   timerSound.currentTime = 0;
