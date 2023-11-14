8a053fe32b45b836b5ae01f23f591d663f046d1d
Melle1986 <melledom@googlemail.com>
2023-11-14 08:21:29 +0100

    update

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ let currTile;
 let otherTile;
 let turns = 0;
 let timerExpired = false;
-
 function moveStart() {
   if (this.style.border.includes("green")) {
     return;
@@ -12,17 +11,13 @@ function moveStart() {
   currTile = this;
   this.style.border = "1px solid black";
 }
-
 function moveOver(e) {
   e.preventDefault();
 }
-
 function moveEnter(e) {
   e.preventDefault();
 }
-
 function moveLeave() {}
-
 function moveDrop() {
   otherTile = this;
 }
@@ -39,27 +34,6 @@ function moveEnd() {
   checkImagePlacement(otherTile);
   checkPuzzleSolved();
 }
-
-function checkImagePlacement(image) {
-  const imageName = image.src.split("/").pop();
-  const imageOrder = parseInt(imageName.split(".")[0], 10);
-  const boardIndex = Array.from(
-    document.getElementById("board").children
-  ).indexOf(image);
-  if (imageOrder === boardIndex + 1) {
-    image.style.border = "1.5px solid green";
-    // Bild ist an der richtigen Stelle
-    image.removeEventListener("dragstart", moveStart);
-    image.removeEventListener("dragend", moveEnd);
-    image.removeEventListener("dragover", moveOver);
-    image.removeEventListener("dragenter", moveEnter);
-    image.removeEventListener("drop", moveDrop);
-  } else {
-    image.style.border = "1.5px solid red";
-    // Bild ist an der falschen Stelle
-  }
-}
-
 window.onload = function () {
   for (let r = 0; r < rows; r++) {
     for (let c = 0; c < columns; c++) {
@@ -92,7 +66,25 @@ window.onload = function () {
   }
   startTimer(330);
 };
-
+function checkImagePlacement(image) {
+  const imageName = image.src.split("/").pop();
+  const imageOrder = parseInt(imageName.split(".")[0], 10);
+  const boardIndex = Array.from(
+    document.getElementById("board").children
+  ).indexOf(image);
+  if (imageOrder === boardIndex + 1) {
+    image.style.border = "1.5px solid green";
+    // Bild ist an der richtigen Stelle
+    image.removeEventListener("dragstart", moveStart);
+    image.removeEventListener("dragend", moveEnd);
+    image.removeEventListener("dragover", moveOver);
+    image.removeEventListener("dragenter", moveEnter);
+    image.removeEventListener("drop", moveDrop);
+  } else {
+    image.style.border = "1.5px solid red";
+    // Bild ist an der falschen Stelle
+  }
+}
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -100,48 +92,13 @@ function shuffleArray(array) {
   }
   return array;
 }
-function startTimer(duration) {
-  let timer = duration,
-    minutes,
-    seconds;
-  let interval = setInterval(function () {
-    minutes = parseInt(timer / 60, 10);
-    seconds = parseInt(timer % 60, 10);
-    document.getElementById("timer").innerText =
-      minutes + "m " + seconds + "s ";
-    if (--timer < 0) {
-      timer = duration;
-      clearInterval(interval);
-      timerExpired = true; // Timer ist abgelaufen
-      document.getElementById("message").style.display = "block";
-    // in die function timer mit einbauen damit sound spielt wenn timer abgelaufen ist
-      document.getElementById("timerSound").play("bing.mp3");
-    }
-  }, 1000);
-}
-startTimer(330);
-removeDragEventListeners();
-
-// function komplett einsetzen damit der sound abgespielt wird wenn timer abgelaufen ist
-
+// Let komplett einsetzen damit der sound abgespielt wird
+// let timerSound = document.getElementById("timerSound");
 function playSound() {
   timerSound.currentTime = 0;
   timerSound.play("bing.mp3");
-  // Eine Event-Listener entfernen, um nur einmal abzuspielen errinerung gesezt da 
-  // ich es immer zwei mal drinne hatte und der mehr mals abgespielt wurde 
+  // Event-Listener entfernen, um nur einmal abzuspielen
   timerSound.removeEventListener("ended", playSound);
-}
-
-
-// Alles dafür da das, das original Bild anzeigt wird 
-// wen man den button geklickt hält 
-
-function removeDragEventListeners() {
-  const tiles = document.querySelectorAll("img");
-  tiles.forEach(function (tile) {
-    tile.removeEventListener("dragstart", moveStart);
-    tile.removeEventListener("dragend", moveEnd);
-  });
 }
 let reloadButton = document.getElementById("reloadButton");
 reloadButton.addEventListener("click", function () {
@@ -162,7 +119,6 @@ function toggleImage() {
 toggleButton.addEventListener("mousedown", toggleImage);
 toggleButton.addEventListener("mouseup", toggleImage);
 let timerInterval;
-
 function startTimer(duration) {
   var timer = duration,
     minutes,
